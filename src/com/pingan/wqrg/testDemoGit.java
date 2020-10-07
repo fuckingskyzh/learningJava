2b476f3031ba072904d61260fdb11457813301cf
gitTestUser <2686712923@qq.com>
2020-10-07 18:22:02 +0800

    kakakakakka啊哈哈哈哈

--- src/com/pingan/wqrg/testDemoGit.java
+++ src/com/pingan/wqrg/testDemoGit.java
@@ -2,6 +2,6 @@ package com.pingan.wqrg;
 
 public class testDemoGit {
     public static void main(String[] args) {
-        System.out.println("helle word word 苍老师您好您好");
+        System.out.println("helle word word shit shi苍老师");
     }
 }

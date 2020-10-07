be87bf5ab9a814947948d413f68b121d0547247c
EX-003 <710330127@qq.com>
2020-10-07 18:10:03 +0800

    演示代码冲突2020107-1-2

--- src/com/pingan/wqrg/testDemoGit.java
+++ src/com/pingan/wqrg/testDemoGit.java
@@ -2,6 +2,6 @@ package com.pingan.wqrg;
 
 public class testDemoGit {
     public static void main(String[] args) {
-        System.out.println("helle word word");
+        System.out.println("helle word word 苍老师您好");
     }
 }
